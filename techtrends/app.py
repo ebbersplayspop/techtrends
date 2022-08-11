19da6e8d9f1e15c6fc0ad8d386589036c961d1f4
Banana Man <ebblewk@yahoo.com>
2022-08-11 21:39:24 +0100

    ##5 Edited app.py to coorect the metrics endpoint to try and make the database stats working correctly

--- techtrends/app.py
+++ techtrends/app.py
@@ -50,7 +50,7 @@ def metrics():
     changes = connection.total_changes
     connection.close()
     response = app.response_class(
-            response=json.dumps({"status":"success","code":0,"data":{"db_connection_count": changes,"post_count": posts}}),
+            response=json.dumps({"db_connection_count": changes,"post_count": posts}),
             status=200,
             mimetype='application/json'
     )
